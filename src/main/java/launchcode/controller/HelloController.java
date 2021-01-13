8764f010811bc66ff22634dc4b3f158afcf648a9
michal <kuk.michal3@gmail.com>
2021-01-13 15:04:27 +0100

    add dummy implementation

--- src/main/java/launchcode/controller/HelloController.java
+++ src/main/java/launchcode/controller/HelloController.java
@@ -1,19 +1,43 @@
 package launchcode.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+
+
+    @RequestMapping(value = "/main-page", method = RequestMethod.GET)
     @ResponseBody
     public String hello(){
 
-        return "Hello!";
+        return "<form action=\"/add\" method=\"get\">\n" +
+                "    <label>First Number</label><input type=\"number\" id=\"username\" name=\"username\"> \n" +
+                "    <label>Second Number</label><input type=\"number\" id=\"email\" name=\"email\">\n" +
+                "    <button>Send</button>\n" +
+                "</form>";
 
+    }
+
+    @GetMapping(path="/add")
+    public @ResponseBody Integer addUser(@RequestParam String username, @RequestParam String email) {
+        int fNum = Integer.parseInt(username);
+        int sNum = Integer.parseInt(email);
+        return fNum + sNum;
+    }
+
+    @GetMapping("/{searchPhrase}")
+    @ResponseBody
+    @ResponseStatus(OK)
+    public String findItemsByCategory(@PathVariable("searchPhrase") String searchPhrase) {
+
+        System.out.println(searchPhrase);
+        return searchPhrase.toUpperCase();
     }
 }
